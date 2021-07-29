46acd4a4a83e522e2d6279b30e61b13cbcd7750a
italo-pessoa-silva <80974829+italo-pessoa-silva@users.noreply.github.com>
2021-07-29 00:34:52 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,5 @@
+
+
+alert(`O seu nome é ${nome}, e você e um fraco que nao troca no x1 ${anoNascimento}, e em 2021 você vai completar 10 x1 perdidos  ${number1-number2} 
+anos nao e recomendado vc jogar mais x1 com o kof scap ` )
+
